00e1c552092aff265a6422e1fc724e3e9fa8d467
L <252152676@qq.com>
2013-05-17 03:23:05 +0800

    Revert "sina"
    This reverts commit 3f6306c87ffeab353376a492fbf9970887f202bb.

--- SinaWEIBO/AppDelegate.h
+++ SinaWEIBO/AppDelegate.h
@@ -8,22 +8,8 @@
 
 #import <UIKit/UIKit.h>
 
-//#define kAppKey @"3639664697"
-//#define kAppSecret @"dfe663094b6b28fc1ca8bf6f3d1d3473"
-//#define kAppRedirectURI @"www.im20.com.cn"
-#define kAppKey @"3298532718"
-#define kAppSecret @"211dee8153bdd59dfb4f8034bb8cc546"
-#define kAppRedirectURI @"http://www.chihao.com/index.php?app=home&mod=public&act=callback"
-@class SinaWeibo;
-@class HomeViewController;
-
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
-{
-    SinaWeibo *sinaweibo;
-}
 
-@property (strong, nonatomic) HomeViewController *viewController;
-@property (readonly, nonatomic) SinaWeibo *sinaweibo;
 @property (strong, nonatomic) UIWindow *window;
 
 @end
